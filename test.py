fa7a5fddd6baa4febaebb6313a50ad1e13d548b4
MaxWarman <maksymilian_gorski@wp.pl>
2023-12-05 12:29:28 +0100

    Fixed time testing

--- test.py
+++ test.py
@@ -7,14 +7,15 @@ def test_time(test_result_directory):
 
     CORRECT_SAMPLES = 44
     TEST_CLIENT_PROFILE_FILEPATH = "./server_db/1.json"
-    TESTS_FOR_NUMBER = 10
-    NUMBERS_OF_UNLOCKING_ROUNDS = [5, 50, 500, 5000, 50000, 500000, 5000000, 50000000, 500000000, 5000000000]
+    TESTS_FOR_NUMBER = 25
+    NUMBERS_OF_UNLOCKING_ROUNDS = [5, 50, 500, 5000, 50000, 500000, 5000000]
     
     with open(f"{test_result_directory}{test_correct_samples_filepath}", "w") as f:
         f.write(f"time;unlocking_rounds\n")
 
-    for i in range(TESTS_FOR_NUMBER):
-        for NUMBER_OF_UNLOCKING_ROUNDS in NUMBERS_OF_UNLOCKING_ROUNDS:
+    time_array = []
+    for NUMBER_OF_UNLOCKING_ROUNDS in NUMBERS_OF_UNLOCKING_ROUNDS:
+        for i in range(TESTS_FOR_NUMBER):
             e = 0
             s = pc()
             try:
@@ -28,20 +29,22 @@ def test_time(test_result_directory):
                 if os.path.exists(TEST_CLIENT_PROFILE_FILEPATH):
                     os.remove(TEST_CLIENT_PROFILE_FILEPATH)
 
-            with open(f"{test_result_directory}{test_correct_samples_filepath}", "a") as f:
-                f.write(
-                    f"{e-s};{NUMBER_OF_UNLOCKING_ROUNDS}\n"
-                )
+            time_array.append(e-s)
 
-                print(f"####### Test for {NUMBER_OF_UNLOCKING_ROUNDS} completed... #######")
+        with open(f"{test_result_directory}{test_correct_samples_filepath}", "a") as f:
+            f.write(
+                f"{sum(time_array)/len(time_array)};{NUMBER_OF_UNLOCKING_ROUNDS}\n"
+            )
+
+            print(f"####### Test for {NUMBER_OF_UNLOCKING_ROUNDS} completed... #######")
     
 def test_correct_samples(test_result_directory):
-    NUMBER_OF_UNLOCKING_ROUNDS = 5000
-    test_correct_samples_filepath = f"test_correct_samples_{NUMBER_OF_UNLOCKING_ROUNDS}_32bit_small_template.csv"
+    NUMBER_OF_UNLOCKING_ROUNDS = 5
+    test_correct_samples_filepath = f"test_correct_samples_{NUMBER_OF_UNLOCKING_ROUNDS}_32bit.csv"
 
     START_CORRECT_SAMPLES = 8
     END_CORRECT_SAMPLES = 44
-    TESTS_FOR_SAMPLE = 1
+    TESTS_FOR_SAMPLE = 25
     TEST_CLIENT_PROFILE_FILEPATH = "./server_db/1.json"
 
     SAMPLES_RANGE = range(START_CORRECT_SAMPLES, END_CORRECT_SAMPLES + 1)
